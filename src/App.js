607590a6af3d3ee09d69f2fdecb8d642991b46ab
Urooj Usman <57406541+UroojUsman@users.noreply.github.com>
2021-03-15 21:03:21 +0500

    FetchApidemo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [repos,setRepos]= useState([{}]);
+
+  useEffect(()=>{
+    async function getData(){
+      const response=await fetch('https://api.github.com/users/UroojUsman/repos')
+   const data = await response.json();
+    console.log(data);
+    setRepos(data);
+    }
+   getData();
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>All Repositories of Urooj Usman</h1>
+        <ul>
+          {repos.map((reposObj,ind)=>{
+            return(<li key={ind}>
+              {reposObj.name}
+              </li>)
+          })}
+        </ul>
       </header>
     </div>
   );
